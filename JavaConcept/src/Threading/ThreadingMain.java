fed97fea5b2d63b146dde1c6718ff4e2c11b6551
VLIKHBA <battula.likhit@pearson.com>
2026-02-26 13:01:39 +0530

    rebase

--- JavaConcept/src/Threading/ThreadingMain.java
+++ JavaConcept/src/Threading/ThreadingMain.java
@@ -1,5 +1,13 @@
 package Threading;
 
+<<<<<<< HEAD
+=======
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
+>>>>>>> 8a418f4 (d)
 //Extending the Thread class
 class ThreadExtends extends Thread{
     @Override
@@ -15,7 +23,11 @@ class ThreadImplements implements Runnable{
     }
 }
 public class ThreadingMain {
+<<<<<<< HEAD
     public static void main(String[] args) {
+=======
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+>>>>>>> 8a418f4 (d)
         ThreadExtends t1 = new ThreadExtends();
         t1.start();
 
@@ -28,6 +40,21 @@ public class ThreadingMain {
             System.out.println("Lambda Thread is running:"+Thread.currentThread().getName());
         });
         threadLambda.start();
+<<<<<<< HEAD
+=======
+
+        //Using Callable with FutureTask
+        Callable<Integer> callthreads= () -> {
+            System.out.println("Callable Thread is running:"+Thread.currentThread().getName());
+            return 42;
+        };
+        //FutureTask is a wrapper that lets you run a Callable in a thread and retrieve the result later. The get() method blocks the main thread until the result is available.
+        FutureTask<Integer> futureTask = new FutureTask<>(callthreads);
+        Thread callthread= new Thread(futureTask);
+        callthread.start();
+        System.out.println("Result from Callable: " + futureTask.get());
+
+>>>>>>> 8a418f4 (d)
         System.out.println("Thread running:"+ Thread.currentThread().getName());
     }
 }
